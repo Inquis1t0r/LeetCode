b6be94a4204db3a8f13ac93c6703ddc998e3f4e2
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-01-02 19:32:59 +0100

    Update RichestCustomerWealth.cs

--- CSharp/RichestCustomerWealth.cs
+++ CSharp/RichestCustomerWealth.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int MaximumWealth(int[][] accounts) {
-        
+        int result = 0; 
+        for (int n = 0; n < accounts.Length; n++) { 
+            System.Console.Write("Row({0}): ", n); 
+            
+            for (int k = 0; k < accounts[n].Length; k++) { 
+                System.Console.Write("{0} ", accounts[n][k]); 
+            } 
+            System.Console.WriteLine(); 
+            return result;
+        } 
     }
 }
