540689e1a19ea4dcc1ca22ebfb2a715d1d4d5d3f
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-01-10 16:52:45 +0100

    Update XOROperationinanArray.cs

--- CSharp/XOROperationinanArray.cs
+++ CSharp/XOROperationinanArray.cs
@@ -1,14 +1,12 @@
-using System.Linq;
-
 public class Solution {
     public int XorOperation(int n, int start) {
         int[] nums = new int[n];
-        
-        for(int i = nums.Length ; i<0; i--){
+        int result = 0;
+        for(int i = 0 ; i<nums.Length; i++){
             nums[i] = start + 2*i;
+            result ^= nums[i];
         }
-        
-        int result = nums.Aggregate((x, y) => x ^ y);
+
       return result;
     }
 }
