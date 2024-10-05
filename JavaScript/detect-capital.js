7761d7941d781e18ac948fefc4d81ff5b99b5315
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-10-05 18:06:41 +0200

    Update detect-capital.js

--- JavaScript/detect-capital.js
+++ JavaScript/detect-capital.js
@@ -3,5 +3,12 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
+    const firstTwoLetters = word.slice(0, 2);
+    const wholeWordToUpper = word.toUpperCase();
     
+    if(!firstTwoLetters == firstTwoLetters.toUpperCase()){
+        return false;
+    }else{
+        return word == wholeWordToUpper;
+    }
 };
