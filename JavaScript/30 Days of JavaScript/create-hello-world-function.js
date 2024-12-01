799c5d3ef6362a59dd9e3801a1d54255d977af38
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-01 18:17:04 +0100

    Create create-hello-world-function.js

---
+++ JavaScript/30 Days of JavaScript/create-hello-world-function.js
@@ -0,0 +1,16 @@
+// 2667. Create Hello World Function
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
