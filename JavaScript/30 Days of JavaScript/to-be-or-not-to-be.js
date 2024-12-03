6bbba7aa19c845999547192facf7ea9d2e96b45a
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-03 15:01:33 +0100

    Create to-be-or-not-to-be.js

---
+++ JavaScript/30 Days of JavaScript/to-be-or-not-to-be.js
@@ -0,0 +1,13 @@
+// 2704. To Be Or Not To Be
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
