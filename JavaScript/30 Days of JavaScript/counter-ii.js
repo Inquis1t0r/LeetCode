1a07c0d976a17bdb305e655c303968a1e335d997
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-05 15:37:54 +0100

    Update counter-ii.js

--- JavaScript/30 Days of JavaScript/counter-ii.js
+++ JavaScript/30 Days of JavaScript/counter-ii.js
@@ -5,7 +5,21 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    
+    let currentValue = init;
+        return {
+        increment: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function() {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    };
 };
 
 /**
