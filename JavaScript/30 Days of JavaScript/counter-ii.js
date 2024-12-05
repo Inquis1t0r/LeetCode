da0f87ab7198c70c51a035321774a222ccec22f1
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-05 15:37:40 +0100

    Create counter-ii.js

---
+++ JavaScript/30 Days of JavaScript/counter-ii.js
@@ -0,0 +1,16 @@
+// 2665. Counter II
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
