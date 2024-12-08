cc552b959d9bc9fca7ce73f58cd9946ae3de8c18
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-08 08:46:52 +0100

    Update apply-transform-over-each-element-in-array.js

--- JavaScript/30 Days of JavaScript/apply-transform-over-each-element-in-array.js
+++ JavaScript/30 Days of JavaScript/apply-transform-over-each-element-in-array.js
@@ -6,5 +6,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
 };
