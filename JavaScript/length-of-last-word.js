e6d3491dfba41c696fcbf90bba9ce1c246093037
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-02-01 11:30:24 +0100

    Update length-of-last-word.js

--- JavaScript/length-of-last-word.js
+++ JavaScript/length-of-last-word.js
@@ -3,5 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+    let arr = s.split(' ');
+    const filteredArr = arr.filter(str => str.trim() !== "");
+    return filteredArr[filteredArr.length-1].length;
 };
