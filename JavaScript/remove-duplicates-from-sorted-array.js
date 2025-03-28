92aa8a9c74ee41447d8e373a2b9e82c88b383888
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-03-28 18:30:31 +0100

    Update remove-duplicates-from-sorted-array.js

--- JavaScript/remove-duplicates-from-sorted-array.js
+++ JavaScript/remove-duplicates-from-sorted-array.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    let uniqueArray = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let exists = false;
+
+        for (let j = 0; j < uniqueArray.length; j++) {
+            if (nums[i] === uniqueArray[j]) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            uniqueArray.push(nums[i]);
+        }
+    }
+    return uniqueArray;
 };
