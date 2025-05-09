7eb4ee1fe701fac490429a92ef4c5c7c2d02f21e
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-05-09 15:04:32 +0200

    Update number-of-equivalent-domino-pairs.js

--- JavaScript/number-of-equivalent-domino-pairs.js
+++ JavaScript/number-of-equivalent-domino-pairs.js
@@ -4,4 +4,8 @@
  */
 var numEquivDominoPairs = function(dominoes) {
     //TODO: Verify input + logic to "rotate" domino 
+    let counter = 0;
+    console.log(dominoes);
+
+    return counter;
 };
