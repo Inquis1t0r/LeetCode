544e5b0f6edbf368fd19b30d72263a1e47b22f4c
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-07-02 05:39:19 +0200

    Update single-number.js

--- JavaScript/single-number.js
+++ JavaScript/single-number.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+    const count = {};
+    nums.forEach(num => {
+        count[num] = (count[num] || 0) + 1;
+    });
+
+    const minCount = Math.min(...Object.values(count));
+    const firstMin = nums.find(item => count[item] === minCount);
+    console.log(firstMin);
+    return firstMin;
 };
