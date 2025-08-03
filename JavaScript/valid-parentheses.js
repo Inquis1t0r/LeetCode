e13ec92e0c6841ea17346b80d12d991db8792fc8
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-08-03 12:01:47 +0200

    Update valid-parentheses.js

--- JavaScript/valid-parentheses.js
+++ JavaScript/valid-parentheses.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const matching = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
     
+    for (let char of s) {
+        if (char in matching) {
+            const top = stack.length > 0 ? stack.pop() : '#';
+            if (top !== matching[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
 };
