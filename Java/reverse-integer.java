6b7ee4cf67cc7fbf147ea527b3fab565a97af813
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-11-30 16:51:54 +0100

    Update reverse-integer.java

--- Java/reverse-integer.java
+++ Java/reverse-integer.java
@@ -1,5 +1,6 @@
 class Solution {
     public int reverse(int x) {
-        
+        String temp = String.valueOf(x);
+        return Integer.valueOf(new StringBuilder(temp).reverse().toString());
     }
 }
