e023ee76fb17e2e8047718a2f26ef0b957220693
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-01-29 08:31:40 +0100

    Update two-sum.java

--- Java/two-sum.java
+++ Java/two-sum.java
@@ -3,8 +3,8 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> seen = new HashMap<>();
-        
+    Map<Integer, Integer> seen = new HashMap<>(nums.length, 1.0f);
+
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
