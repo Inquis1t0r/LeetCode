00a6a6edf570be331aa5148a77b0af233926d190
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-01-29 08:33:51 +0100

    Refactor twoSum method for improved readability

--- Java/two-sum.java
+++ Java/two-sum.java
@@ -4,22 +4,24 @@ import java.util.Map;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-    if (nums == null || nums.length < 2) {
-        return new int[] {};
-    }
+        if (nums == null || nums.length < 2) {
+            return new int[] {};
+        }
 
-    Map<Integer, Integer> seen = new HashMap<>(nums.length, 1.0f);
+        Map < Integer, Integer > seen = new HashMap < > (nums.length, 1.0 f);
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            
+
             if (seen.containsKey(complement)) {
-                return new int[] { seen.get(complement), i };
+                return new int[] {
+                    seen.get(complement), i
+                };
             }
-            
+
             seen.put(nums[i], i);
         }
-        
+
         return new int[] {};
     }
 }
