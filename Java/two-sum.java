32e64a395e7c9bf2dbdf0b30e9b93209053b9636
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-01-29 08:28:59 +0100

    Update two-sum.java

--- Java/two-sum.java
+++ Java/two-sum.java
@@ -1,15 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] == target - nums[i]) {
-                        return new int[] { i, j };
-                }
-            }
-    }
+        Map<Integer, Integer> seen = new HashMap<>();
         
-   return null; 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (seen.containsKey(complement)) {
+                return new int[] { seen.get(complement), i };
+            }
+            
+            seen.put(nums[i], i);
+        }
+        
+        return new int[] {}; 
     }
- 
 }
