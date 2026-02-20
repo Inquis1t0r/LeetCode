768b3ca51177dc9e0c9b70de6c8bfb479ed68406
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-02-20 18:47:52 +0100

    Implement in-place string reversal

--- JavaScript/reverse-string.js
+++ JavaScript/reverse-string.js
@@ -3,5 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-   (s) => s.split('').reverse().join('');
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        
+        left++;
+        right--;
+    }
 };
