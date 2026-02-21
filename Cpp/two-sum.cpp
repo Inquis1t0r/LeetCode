b21c08c603603a57e0330b515fef22da4d766ffa
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-02-21 16:03:17 +0100

    Create two-sum.cpp

---
+++ Cpp/two-sum.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        if(nums.size() < 2){
+            return vector<int>();
+        }
+        return vector<int>();
+    }
+};
